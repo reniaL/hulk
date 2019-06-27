fe798e0c3cd42ed5e6552746e462562720ec5e86
reniaL <renial@163.com>
2019-06-27 17:28:05 +0800

    update json_demo

--- lang/json_demo.py
+++ lang/json_demo.py
@@ -2,13 +2,25 @@
 import json
 
 
-TIMEOUT = 10
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-def parse():
+
+def str_to_json():
     content = '[{"name": "Terry", "age": 25}, {"name": "Mary", "age": 20}]'
-    l = json.loads(content)
-    print len(l)
-    print l[0]
+    jsonobj = json.loads(content)
+    print len(jsonobj)
+    print jsonobj[1]
+
+
+def obj_to_json():
+    person = Person("Terry", 30)
+    jsonobj = json.dumps(person.__dict__)
+    print jsonobj
+
 
 if __name__ == '__main__':
-    parse()
+    # str_to_json()
+    obj_to_json()
